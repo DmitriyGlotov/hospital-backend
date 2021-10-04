cbfc8f9517a33f37c0e46370847c9235102a4f2c
DmitriyGloyov <dglotov.exceedteam@gmail.com>
2021-10-04 11:47:27 +0300

    token-implementation

--- src/modules/controllers/schedule-controller.js
+++ src/modules/controllers/schedule-controller.js
@@ -1,35 +1,41 @@
-const Shedule = require('../../db/models/schadule/shedule');
+const Shedule = require('../../db/models/shedule/shedule');
+const jwt = require("jsonwebtoken");
 
 module.exports.allShedule = (req, res) => {
-  Shedule.find().then(result => {
+  const {authorization} = req.headers;
+  const decoded = jwt.verify(authorization, process.env.JWT_ACCESS_TOKEN);
+  Shedule.find({UserId: decoded.id}).then(result => {
     res.send({ data: result });
   });
 }
 
 module.exports.createShedule = (req, res) => {
-  const shedule = new Shedule(req.body);
-  if (!shedule.name || !shedule.doctor || !shedule.data || !shedule.lament) {
-    res.status(422).send('Error! Data not correct');
-  } else {
-    shedule.save().then(result => {
-      Shedule.find().then(result => {
-        res.send({ data: result });
-      });
+  const { headers, body } = req;
+  const decoded = jwt.verify(headers.authorization, process.env.JWT_ACCESS_TOKEN);
+  body.UserId = decoded.id;
+  const shedule = new Shedule(body);
+  shedule.save().then(result => {
+    Shedule.find({UserId: decoded.id}).then(result => {
+      res.send({ data: result });
     });
-  }
+  });
 };
 
 module.exports.changeShedule = (req, res) => {
-    Shedule.updateOne({_id: req.body._id}, req.body).then(result => {
-      Shedule.find().then(result => {
-        res.send({ data: result });
-      });
+  const { authorization } = req.headers;
+  const decoded = jwt.verify(authorization, process.env.JWT_ACCESS_TOKEN);
+  Shedule.updateOne({_id: req.body._id}, req.body).then(result => {
+    Shedule.find({UserId: decoded.id}).then(result => {
+      res.send({ data: result });
     });
+  });
 };
 
 module.exports.deleteShedule = (req, res) => {
+const { authorization } = req.headers;
+const decoded = jwt.verify(authorization, process.env.JWT_ACCESS_TOKEN);
 Shedule.deleteOne({_id: req.query._id}).then(result => {
-  Shedule.find().then(result => {
+  Shedule.find({UserId: decoded._id}).then(result => {
       res.send({ data: result });
     });
   });
